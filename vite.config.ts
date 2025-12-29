8939103214efa4905b9bd15a4f79cee737bb4a99
filliprvieira-prime <filliprvieira@gmail.com>
2025-12-28 21:13:44 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Certifique-se de que o nome do repositório no GitHub seja exatamente 'atlanticgarden-primeguarapari'
 export default defineConfig({
   plugins: [react()],
   base: '/atlanticgarden-primeguarapari/',
